53aee6f4b1f809eb88d257b1dff41c3148c828cf
Vaashtor <vaashtor@gmail.com>
2025-09-10 20:17:01 +0300

    start of our game!

--- scripts/coin.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-@onready var game_manager: Node = %GameManager
-
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	game_manager.add_point()
-	queue_free()
